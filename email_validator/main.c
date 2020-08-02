528b142db0a27eefd8357cceaa159a6fff1448f5
Benyamin <beneslami72@gmail.com>
2020-08-02 23:21:50 +0430

    email validator application

---
+++ email_validator/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "utils.h"
+
+int main(int argc, char **argv){
+  char *email = argv[1];
+  fsm_bool_t fsm_result;
+  fsm_t *email_fsm = email_validator_fsm();
+  fsm_error_t fsm_error = fsm_execute(email_fsm, email, strlen(email), 0, &fsm_result);
+  if(fsm_error == FSM_NO_ERROR){
+      if(fsm_result == FSM_TRUE){
+        printf("Valid Email\n");
+      }
+      else{
+        printf("Invalid Email\n");
+      }
+  }
+  else{
+    printf("FSM failed\n");
+  }
+  return 0;
+}
