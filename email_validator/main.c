1354f50b2394313053b892a55720a16b12216edc
Benyamin <beneslami72@gmail.com>
2020-08-03 00:41:55 +0430

    update

--- email_validator/main.c
+++ email_validator/main.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv){
   char *email = argv[1];
   fsm_bool_t fsm_result;
   fsm_t *email_fsm = email_validator_fsm();
-  fsm_error_t fsm_error = fsm_execute(email_fsm, email, strlen(email), 0, &fsm_result);
+  fsm_error_t fsm_error = execute_fsm(email_fsm, email, strlen(email), 0, &fsm_result);
   if(fsm_error == FSM_NO_ERROR){
       if(fsm_result == FSM_TRUE){
         printf("Valid Email\n");
